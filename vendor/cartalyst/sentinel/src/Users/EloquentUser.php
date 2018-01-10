d4adc3e34d3dc32d6bcdd68aff3369faba980bd9
psycadelik <francisadrian9@outlook.com>
2018-01-10 19:52:01 +0300

    initial commit

--- vendor/cartalyst/sentinel/src/Users/EloquentUser.php
+++ vendor/cartalyst/sentinel/src/Users/EloquentUser.php
@@ -11,7 +11,7 @@
  * bundled with this package in the LICENSE file.
  *
  * @package    Sentinel
- * @version    2.0.15
+ * @version    2.0.17
  * @author     Cartalyst LLC
  * @license    BSD License (3-clause)
  * @copyright  (c) 2011-2017, Cartalyst LLC
@@ -114,21 +114,6 @@ class EloquentUser extends Model implements RoleableInterface, PermissibleInterf
     public function getLoginNames()
     {
         return $this->loginNames;
-    }
-
-    public function hasPassword()
-    {
-        return $this->password !== '';
-    }
-
-    public function addresses()
-    {
-        return $this->hasMany('App\Address', 'user_id');
-    }
-
-    public function orders()
-    {
-        return $this->hasMany('App\Order', 'user_id');
     }
 
     /**
